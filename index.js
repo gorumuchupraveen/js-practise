b25edad8d80ff029fdb17fd7ae762faf44e58a4a
killerkoli <gpraveen29041996@gmail.com>
2020-06-12 04:51:49 +0530

    1 file modified

--- index.js
+++ index.js
@@ -10,16 +10,23 @@ let min = date.getMinutes();
 let seconds = date.getSeconds();
 let day = date.getDay();
 
+let addZone = hours >= 12 ? "PM" : "AM";
+seconds = seconds <= 9 ? "0" + seconds : seconds;
+console.log(seconds);
+
+if (seconds >= date.getSeconds(9)) {
+}
+
 let daylist = [
   "Sunday",
   "Monday",
   "Tuesday",
   "Wednesday",
-  "THrusday",
+  "Thrusday",
   "Friday",
   "Saturday",
   "Sunday"
 ];
-console.log('Today is' + ' ' + daylist[day])
+console.log("Today is" + " " + daylist[day]);
 
-console.log(hours + ":" + min + ":" + seconds );
+console.log(hours + ":" + min + ":" + seconds + " " + addZone);
